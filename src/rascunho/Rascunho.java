7de02d20fa3e9f1d70776cc9e5a3a3abef8c06d7
vinicius <vinicius>
2020-02-11 10:35:36 -0300

    Método para recuperar o código da unimed no arquivo

--- src/rascunho/Rascunho.java
+++ src/rascunho/Rascunho.java
@@ -4,9 +4,6 @@ import java.util.Date;
 
 public class Rascunho {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         Date data = new Date();
         System.out.println(data);
