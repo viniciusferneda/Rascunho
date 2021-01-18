5a8090cced1b976c9d25cbfda68d98e70b7e1a7a
Vinicius Ferneda de Lima <vinicius@dynamix.com.br>
2021-01-18 12:01:04 -0300

    Sorteio Lotofacil

--- src/donus/Teste3.java
+++ src/donus/Teste3.java
@@ -1,27 +1,38 @@
 package donus;
 
-import java.util.*;
-
 public class Teste3 {
 
     public static int ArrayChallenge(int[] arr) {
+        Integer[] arraySemRepetidos = new Integer[arr.length];
         int qtdEntradaRepetidas = 0;
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                if(i != j){
-                    if(arr[i] == arr[j]){
-                        qtdEntradaRepetidas++;
-                    }
+        for (int i = 0; i < arr.length; i++) {
+            boolean isContain = false;
+            for (int j = 0; j < arraySemRepetidos.length; j++) {
+                if(arraySemRepetidos[j] != null && arraySemRepetidos[j] == arr[i]){
+                    isContain = true;
+                    qtdEntradaRepetidas++;
                 }
             }
+            if(!isContain){
+                arraySemRepetidos[i] = arr[i];
+            }
         }
+        /*for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                if (arr[i] == arr[i - 1]) {
+                    qtdEntradaRepetidas++;
+                }
+            }
+        }*/
         return qtdEntradaRepetidas;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // keep this function call here
         //Scanner s = new Scanner(System.in);
-        System.out.print(ArrayChallenge(new int[] {0,-2,-2,5,5,5}));
+        System.out.println(ArrayChallenge(new int[]{0, -2, -2, 5, 5, 5}));
+        System.out.println(ArrayChallenge(new int[]{0, 2, 2, 2, 5}));
+        System.out.println(ArrayChallenge(new int[]{0, 2, 5}));
     }
 
 }
