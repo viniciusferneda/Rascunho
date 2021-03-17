2e8f683fcd8c4a89fb7f7d2d21f15a9ba318f836
Vinicius Ferneda de Lima <vinicius@dynamix.com.br>
2021-03-17 16:57:24 -0300

    Introdução a Kotlin

--- src/kotlin/IntroducaoKotlin.kt
+++ src/kotlin/IntroducaoKotlin.kt
@@ -1,55 +1,56 @@
 package kotlin
 
-const val CONS_INT_MAX_VALUE = Int.MAX_VALUE
-
 class IntroducaoKotlin {
 
-    fun main() {
+    companion object {
+        const val CONS_INT_MAX_VALUE = Int.MAX_VALUE
 
-        val pi = 3.14159265358979323
-        val formatted = "Pi: %.2f".format(pi)
-        print(formatted)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val pi = 3.14159265358979323
+            val formatted = "Pi: %.2f".format(pi)
+            print(formatted)
 
-        var currentAge = 22 // a variável assume o tipo a partir do primeiro valor inserido
-        var currentAgeTipo:Int? //currentAge = null ou 22
-        currentAgeTipo = 23
+            var currentAge = 22 // a variável assume o tipo a partir do primeiro valor inserido
+            var currentAgeTipo: Int? //currentAge = null ou 22
+            currentAgeTipo = 23
 
-        val currentAgeVal = 24 // a variável final assume o tipo a partir do primeiro valor inserido
-        val currentAgeValTipo:Int? //currentAge = null ou 22
-        currentAgeValTipo = 25
+            val currentAgeVal = 24 // a variável final assume o tipo a partir do primeiro valor inserido
+            val currentAgeValTipo: Int? //currentAge = null ou 22
+            currentAgeValTipo = 25
 
-        println(currentAge)
-        println(currentAgeTipo)
-        println(currentAgeVal)
-        println(currentAgeValTipo)
-        println(CONS_INT_MAX_VALUE)
+            println(currentAge)
+            println(currentAgeTipo)
+            println(currentAgeVal)
+            println(currentAgeValTipo)
+            println(CONS_INT_MAX_VALUE)
 
-        /* ERRO - Precisar partir de uma referência
+            /* ERRO - Precisar partir de uma referência
         var currentAgeError
         currentAgeError = 22
         */
 
-        /* ERRO - Variável inicializada como string, não pode atribuir valor diferente do tipo definido
+            /* ERRO - Variável inicializada como string, não pode atribuir valor diferente do tipo definido
         var currentYear = "Ano"
         currentYear = 2021
         */
 
-        //Atribui valor null corretamente a month
-        var month:Int? = null
+            //Atribui valor null corretamente a month
+            var month: Int? = null
 
-        /* ERRO de compilação
+            /* ERRO de compilação
         var day:Int = null
         */
 
-        val welcome = "Bem vindo"
-        var name = "Vinícius"
-        println("$welcome, ${name.capitalize()}!")
+            val welcome = "Bem vindo"
+            var name = "Vinícius"
+            println("$welcome, ${name.capitalize()}!")
 
-        val x = calculate(12,4,::sum)
-        val y = calculate(12,4){a,b -> a*b}
-        println("X = ${x}, Y = ${y}")
+            val x = calculate(12, 4, ::sum)
+            val y = calculate(12, 4) { a, b -> a * b }
+            println("X = ${x}, Y = ${y}")
 
-        /* switch
+            /* switch
         when {
             case1 -> {}
             case2 -> {}
@@ -62,38 +63,39 @@ class IntroducaoKotlin {
         }
         */
 
-        val num1:Int? = null
-        var num2 = num1 ?: 0
-        println(num1)
-        println(num2)
+            val num1: Int? = null
+            var num2 = num1 ?: 0
+            println(num1)
+            println(num2)
 
-        println("For com step")
-        for(i in 0..20 step 2){
-            print(i)
+            println("For com step")
+            for (i in 0..20 step 2) {
+                print(i)
+            }
+            println("")
+
+            println("For com until")
+            for (i in 0 until 10) {
+                print(i)
+            }
+            println("")
+
+            println("For com downTo")
+            for (i in 10 downTo 0) {
+                print(i)
+            }
+            println("")
+
+            val sArray = "Olha essa string!"
+            for (s in sArray) {
+                print("$s ")
+            }
         }
-        println("")
 
-        println("For com until")
-        for(i in 0 until 10){
-            print(i)
+        fun sum(a1: Int, a2: Int) = a1.plus(a2)
+
+        fun calculate(n1: Int, n2: Int, operation: (Int, Int) -> Int): Int {
+            return operation(n1, n2)
         }
-        println("")
-
-        println("For com downTo")
-        for(i in 10 downTo 0){
-            print(i)
-        }
-        println("")
-
-        val sArray = "Olha essa string!"
-        for(s in sArray){
-            print("$s ")
-        }
-    }
-
-    fun sum(a1:Int, a2:Int) = a1.plus(a2)
-
-    fun calculate(n1:Int,n2:Int,operation:(Int,Int) ->Int):Int{
-        return operation(n1,n2)
     }
 }
